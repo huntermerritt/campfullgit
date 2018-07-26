8535a20b202248727c5a60f7eda9917f247f97bd
Hunter <hunteramerritt1997@gmail.com>
2018-07-25 22:16:44 -0500

    Added favicon

--- lib/campfull_web/endpoint.ex
+++ lib/campfull_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule CampfullWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :campfull, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt .well-known)
+    only: ~w(css fonts images js myfavicon.ico robots.txt .well-known)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
